8ed5e8ecdf3374a53b366264d2d7e6ba52331a03
Sakinah Cottman <sakinah.cottman@yahoo.com>
2019-08-05 09:35:34 -0400

    attempt

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -3,6 +3,20 @@ package hbcu.stay.ready.algor;
 public class Solution {
 
     public String countUniqueWords(String input){
+        String [] sentence = input.split(" ");
+        int count = 0;
+        for(int x = 0; x<sentence.length; x++){
+            for(int i = 0; i<sentence.length; i++){
+                if(sentence[x].equals(sentence[i])){
+                    count +=1;
+                    sentence[x] = "0";
+                }
+                if(sentence[x]!="0"){
+                    System.out.println(count);
+                }
+
+            }
+        }
         return null;
     }
 }
