62cb2fbefc2a6e9a01d208a2d6c5a2de78d89ac2
Sakinah Cottman <sakinah.cottman@yahoo.com>
2019-08-06 08:23:17 -0400

    Got it to print out how many words there were but a random number was included in the first test

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,22 +1,38 @@
 package hbcu.stay.ready.algor;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Solution {
 
     public String countUniqueWords(String input){
-        String [] sentence = input.split(" ");
+        String [] sentence = input.split("\\.|\\!|\\s");
         int count = 0;
-        for(int x = 0; x<sentence.length; x++){
-            for(int i = 0; i<sentence.length; i++){
-                if(sentence[x].equals(sentence[i])){
-                    count +=1;
-                    sentence[x] = "0";
-                }
-                if(sentence[x]!="0"){
-                    System.out.println(count);
-                }
 
+        Map<String, Integer> words = new HashMap<String, Integer>();
+        for(int x = 0; x<sentence.length; x++){
+            count = 0;
+            for(int j = 0; j<sentence.length; j++){
+                if(sentence[x].equals(sentence[j])){
+                    count++;
+                }
             }
+            words.put(sentence[x],count);
         }
-        return null;
+
+
+        /*
+        for(int x = 0; x<sentence.length; x++){
+            count = 1;
+
+        }
+        for(int i = 1; i<sentence.length; i++){
+            if(sentence[x])
+        }
+
+         */
+
+        return String.valueOf(words);
     }
 }
